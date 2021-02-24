d5df2051651587b8888a7efe9afc672432053125
Ramchandran Muthukumar <ramcha1994@gmail.com>
2021-02-24 16:04:05 -0500

    deployed simple backend via heroku

--- src/main/java/backend/HelloWorld.java
+++ src/main/java/backend/HelloWorld.java
@@ -3,7 +3,18 @@ package backend;
 import static spark.Spark.*;
 
 public class HelloWorld {
+    private static int getHerokuAssignedPort() {
+        // Heroku stores port number as an environment variable
+        String herokuPort = System.getenv("PORT");
+        if (herokuPort != null) {
+            return Integer.parseInt(herokuPort);
+        }
+        //return default port if heroku-port isn't set (i.e. on localhost)
+        return 4567;
+    }
+
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         get("/", (req, res) -> "Welcome to Group 10's project");
     }
 }
