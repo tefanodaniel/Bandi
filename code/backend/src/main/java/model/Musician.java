c7b94c29232858874a43ee17ace80bbac28c391e
Elizabeth Aguirre <eaguirr3@jhu.edu>
2021-03-04 22:19:19 -0500

    added getters/setters, toString(), hashCode(), and equals() to Musician

--- code/backend/src/main/java/model/Musician.java
+++ code/backend/src/main/java/model/Musician.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Musician extends Client {
 
     private String name;
@@ -19,5 +21,60 @@ public class Musician extends Client {
         this.genre = genre;
         this.instrument = instrument;
         this.experience = experience;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getInstrument() {
+        return instrument;
+    }
+
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Musician musician = (Musician) o;
+        return name.equals(musician.name) && genre.equals(musician.genre) && Objects.equals(instrument, musician.instrument) && Objects.equals(experience, musician.experience);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, genre, instrument, experience);
+    }
+
+    @Override
+    public String toString() {
+        return "Musician{" +
+                "name='" + name + '\'' +
+                ", genre='" + genre + '\'' +
+                ", instrument='" + instrument + '\'' +
+                ", experience='" + experience + '\'' +
+                '}';
     }
 }
