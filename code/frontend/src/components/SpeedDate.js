a898b52ac96a5eb60766f2623db682992d309e80
Max Torres <maxtorres487@gmail.com>
2021-03-30 21:23:58 -0400

    Added logic to display event page differently if one is an admin

--- code/frontend/src/components/SpeedDate.js
+++ code/frontend/src/components/SpeedDate.js
@@ -7,6 +7,8 @@ import Cookies from "js-cookie";
 import { connect } from 'react-redux';
 import { fetchSDEvents } from '../actions/sd_event_actions';
 import SDEventApi from "../utils/SDEventApiService";
+import Button from "react-bootstrap/Button";
+import {fetchBandsForMusician} from "../actions/band_actions";
 
 class SpeedDate extends React.Component {
     constructor(props) {
@@ -21,28 +23,58 @@ class SpeedDate extends React.Component {
     componentDidMount() {
         SDEventApi.getAll()
             .then((response) =>
-            this.setState({sdEvents : response.data}));;
+            this.setState({sdEvents : response.data}));
     }
 
     render() {
-
         console.log(this.state.sdEvents);
 
-        return (
-            <div className="bg-transparent" style={bandi_styles.discover_background}>
-            <Header/>
-            <SubHeader text={"Register for speed-dating events here!"}/>
+        const userInfo = this.props.store.user_reducer;
+        const isAdmin = userInfo.admin;
+        console.log('here here', userInfo);
 
-            <h1>Speed-Dating Events:</h1>
-
+        var eventList = this.state.sdEvents.map((event) =>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{event.name}</h5>
+                    <h6 className="card-subtitle">{event.date}</h6>
+                    <h6 className="card-subtitle"><a href={event.link}>{event.link}</a></h6>
+                    <p className="card-text">Minimum number of participants: {event.minusers}</p>
+                    <Button className="small font-italic" onClick={() => {}}>Register</Button>
+                </div>
             </div>
-        )
+        );
+
+        if (isAdmin) {
+            return(
+                <div className="bg-transparent" style={bandi_styles.discover_background}>
+                    <Header/>
+                    <SubHeader text={"Register for speed-dating events here!"}/>
+
+                    <h1>Speed-Dating Events:</h1>
+                    {eventList}
+
+                    <h1>Create Event: (Admin Only)</h1>
+                </div>
+            )
+        }
+        else { // not an admin
+            return (
+                <div className="bg-transparent" style={bandi_styles.discover_background}>
+                    <Header/>
+                    <SubHeader text={"Register for speed-dating events here!"}/>
+
+                    <h1>Speed-Dating Events:</h1>
+                    {eventList}
+                </div>
+            )
+        }
     }
 
 }
 
 function mapStateToProps(state) {
-    return {state};
+    return {store:state};
 }
 
 export default connect(mapStateToProps)(SpeedDate);
