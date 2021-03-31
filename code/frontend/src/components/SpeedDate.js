79dd6c9c1a4c8b1b8c197ae44b4b23af48252ea6
Max Torres <maxtorres487@gmail.com>
2021-03-31 11:02:08 -0400

    Created form for speed-dating event creation

--- code/frontend/src/components/SpeedDate.js
+++ code/frontend/src/components/SpeedDate.js
@@ -9,6 +9,8 @@ import { fetchSDEvents } from '../actions/sd_event_actions';
 import SDEventApi from "../utils/SDEventApiService";
 import Button from "react-bootstrap/Button";
 import {fetchBandsForMusician} from "../actions/band_actions";
+import Form from "react-bootstrap/Form";
+import FormGroup from "react-bootstrap/FormGroup";
 
 class SpeedDate extends React.Component {
     constructor(props) {
@@ -16,8 +18,25 @@ class SpeedDate extends React.Component {
 
         this.state = {
             id: Cookies.get('id'),
-            sdEvents: []
+            sdEvents: [],
+
+            name: '',
+            link: '',
+            date: '',
+            minusers: 1
         }
+
+        //this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        let value = target.value;
+        const field = target.name;
+        this.setState({
+            [field]: value
+        });
     }
 
     componentDidMount() {
@@ -54,7 +73,36 @@ class SpeedDate extends React.Component {
                     <h1>Speed-Dating Events:</h1>
                     {eventList}
 
-                    <h1>Create Event: (Admin Only)</h1>
+                    <h1>Create Event: (Admin)</h1>
+
+                    <h1>{this.state.name}{this.state.link}{this.state.date}{this.state.minusers}</h1>
+
+                    <Form onSubmit={this.handleSubmit}>
+
+                        <Form.Group controlId="profileForm.name">
+                            <Form.Label>Name:</Form.Label>
+                            <Form.Control name="name" type="input"  placeholder="Name" value={this.state.name} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Form.Group controlId="profileForm.link">
+                            <Form.Label>Link:</Form.Label>
+                            <Form.Control name="link" type="input"  placeholder="Link" value={this.state.link} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Form.Group controlId="profileForm.date">
+                            <Form.Label>Date:</Form.Label>
+                            <Form.Control name="date" type="input"  placeholder="Date" value={this.state.date} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Form.Group controlId="profileForm.minusers">
+                            <Form.Label>Minimum Number of Participants:</Form.Label>
+                            <Form.Control name="minusers" type="number"  placeholder="1" value={this.state.minusers} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Button type="submit">Create Event</Button>
+
+                    </Form>
+
                 </div>
             )
         }
