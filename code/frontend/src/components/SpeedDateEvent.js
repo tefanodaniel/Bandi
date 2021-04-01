b849232fb1b845d684608bf353cbc42ea3b0bc17
Max Torres <maxtorres487@gmail.com>
2021-04-01 10:57:04 -0400

    added button to toggle leaving and joining an event

--- code/frontend/src/components/SpeedDateEvent.js
+++ code/frontend/src/components/SpeedDateEvent.js
@@ -22,56 +22,103 @@ class SpeedDateEvent extends React.Component {
         this.state = {
 
             id: Cookies.get('id'),
+            isParticipant: false,
+            buttonText: 'Register',
 
             eventId: '',
-            name: '',
+            name: 'Loading...',
             link: '',
             date: '',
             minusers: 1,
-            participants: []
+            participants: [],
+            participantNames: []
         }
+
+        this.eventInfo = this.eventInfo.bind(this);
+        this.register_leave = this.register_leave.bind(this);
+        this.buttonText = this.buttonText.bind(this);
     }
 
-    componentDidMount() {
+    eventInfo() {
         const params = new URLSearchParams(this.props.location.search);
         let eventId = params.get("view");
 
         SDEventApi.get(eventId)
             .then((response) =>
-                this.setState({
-                    name: response.data.name,
-                    eventId: response.data.id,
-                    link: response.data.link,
-                    date: response.data.date,
-                    minusers: response.data.minusers,
-                    participants: response.data.participants}));
+                    this.setState({
+                        name: response.data.name,
+                        eventId: response.data.id,
+                        link: response.data.link,
+                        date: response.data.date,
+                        minusers: response.data.minusers,
+                        participants: response.data.participants},
+
+                function() {
+                    this.setState({isParticipant :
+                            (this.state.participants).indexOf(this.state.id) > -1})
+                        /*
+                    this.setState({participantNames: []});
+
+                    this.state.participants.forEach(id => {
+                        SDEventApi.get(id)
+                            .then((response) =>
+                                this.setState({participantNames:
+                                        this.state.participantNames.concat(response.data.name)})
+                            );
+                    });*/
+                }));
+    }
+
+    register_leave() {
+        this.setState({isParticipant : !(this.state.isParticipant)});
+        this.buttonText();
+    }
+
+    buttonText() {
+        if (this.state.isParticipant) {
+            this.state.buttonText = "Leave";
+        }
+        else {
+            this.state.buttonText = "Register";
+        }
+    }
+
+    componentDidMount() {
+        this.eventInfo();
+        this.setState({isParticipant :
+                (this.state.participants).indexOf(this.state.id) > -1})
+        this.buttonText();
     }
 
     render() {
 
         const userInfo = this.props.store.user_reducer;
         const isAdmin = userInfo.admin;
-        console.log('here here', userInfo);
+        this.buttonText();
 
-        if (isAdmin) {
+        if (!isAdmin) { // non admin view
             return(
                 <div className="bg-transparent" style={bandi_styles.discover_background}>
                     <Header/>
 
-                    <h1>{this.state.name}</h1>
-                    <h2>{this.state.date}</h2>
-                    <h2><a href={this.state.link}>{this.state.link}</a></h2>
-                    <h2>Minimum number of participants: {this.state.minusers}</h2>
-                    <h2>Registered participants: {this.state.participants.length}</h2>
                 </div>
             )
         }
-        else { // not an admin
+        else { // is admin
             return (
                 <div className="bg-transparent" style={bandi_styles.discover_background}>
                     <Header/>
 
-                    <h1>{this.state.name}</h1>
+                    <h1>{this.state.name} (Admin)</h1>
+                    <h2>{this.state.date}</h2>
+                    <h2><a href={this.state.link}>{this.state.link}</a></h2>
+                    <h2>Minimum number of participants: {this.state.minusers}</h2>
+                    <h2>Registered participants: {this.state.participants.length}</h2>
+
+                    <Button onClick={() => {this.register_leave()}}>{this.state.buttonText}</Button>
+                    <h1>{this.state.isParticipant.toString()}</h1>
+
+
                 </div>
             )
         }
