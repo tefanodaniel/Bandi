bd58f01664398e86dcf3339db4aeabf0134ac755
Max Torres <maxtorres487@gmail.com>
2021-04-07 18:13:09 -0400

    Moved SOTW Submission routes to controller file

--- code/backend/src/main/java/api/ApiServer.java
+++ code/backend/src/main/java/api/ApiServer.java
@@ -190,99 +190,12 @@ public class ApiServer {
         put("/songs/:songid", SongController.putSong);
         delete("/songs/:songId", SongController.deleteSong);
 
-        // Song of the Week Submission Api Routes
-        // get (read) all submissions
-        get("/submissions", (req, res) -> {
-            try {
-                List<SongOfTheWeekSubmission> submissions = sotw_submissionDao.readAll();
-                System.out.println(submissions);
-                return gson.toJson(submissions);
-            } catch (DaoException ex) {
-                throw new ApiError(ex.getMessage(), 500);
-            }
-        });
-
-        // get (read) a submission given submissionId
-        get("/submissions/:submissionid", (req, res) -> {
-            try {
-                String submissionid = req.params("submissionid");
-                SongOfTheWeekSubmission s = sotw_submissionDao.read(submissionid);
-                if (s == null) {
-                    throw new ApiError("Resource not found", 404); // Bad request
-                }
-                res.type("application/json");
-                return gson.toJson(s);
-            } catch (DaoException ex) {
-                throw new ApiError(ex.getMessage(), 500);
-            }
-        });
-
-        // post (create) a submission from a user with musician_id
-        post("/submissions", (req, res) -> {
-            try {
-                SongOfTheWeekSubmission submission = gson.fromJson(req.body(), SongOfTheWeekSubmission.class);
-                Set<String> instruments = submission.getInstruments();
-
-                if (instruments == null) { instruments = new HashSet<String>(); }
-                sotw_submissionDao.create(submission.getSubmission_id(), submission.getMusician_id(), submission.getAVSubmission(), instruments);
-                res.status(201);
-                return gson.toJson(submission);
-            } catch (DaoException ex) {
-                throw new ApiError(ex.getMessage(), 500);
-            }
-        });
-
-        // put (update) a submission with info
-        put("/submissions/:submissionid", (req, res) -> {
-            try {
-
-                String submissionid = req.params("submissionid");
-                SongOfTheWeekSubmission submission = gson.fromJson(req.body(), SongOfTheWeekSubmission.class);
-                if (submission == null) {
-                    throw new ApiError("Resource not found", 404);
-                }
-
-                if (! (submission.getSubmission_id().equals(submissionid))) {
-                    throw new ApiError("submission ID does not match the resource identifier", 400);
-                }
-
-                String avsubmission = submission.getAVSubmission();
-                Set<String> instruments = submission.getInstruments();
-
-                // Update specific fields:
-                boolean flag = false;
-                if (avsubmission != null) {
-                    flag = true;
-                    submission = sotw_submissionDao.updateAVSubmission(submissionid, avsubmission);
-                } if (instruments != null) {
-                    flag = true;
-                    submission = sotw_submissionDao.updateInstruments(submissionid, instruments);
-                } if (!flag) {
-                    throw new ApiError("Nothing to update", 400);
-                } if (submission == null) {
-                    throw new ApiError("Resource not found", 404);
-                }
-
-                return gson.toJson(submission);
-            } catch (DaoException | JsonSyntaxException ex) {
-                throw new ApiError(ex.getMessage(), 500);
-            }
-        });
-
-        // delete (remove) a submission
-        delete("/submissions/:submissionid", (req, res) -> {
-            try {
-                String submissionid = req.params("submissionid");
-                SongOfTheWeekSubmission submission = sotw_submissionDao.delete(submissionid);
-                if (submission == null) {
-                    throw new ApiError("Resource not found", 404); // Bad request
-                }
-                res.type("application/json");
-                return gson.toJson(submission);
-            } catch (DaoException ex) {
-                throw new ApiError(ex.getMessage(), 500);
-            }
-        });
+        // SOTW Submission Api Routes
+        get("/submissions", SOTWSubmissionController.getAllSubmissions);
+        get("/submissions/:submissionid", SOTWSubmissionController.getSubmissionById);
+        post("/submissions", SOTWSubmissionController.postSubmission);
+        put("/submissions/:submissionid", SOTWSubmissionController.putSubmission);
+        delete("/submissions/:submissionid", SOTWSubmissionController.deleteSubmission);
 
         // Song of the Week Event Api Routes
         // get (read) all sotw events
