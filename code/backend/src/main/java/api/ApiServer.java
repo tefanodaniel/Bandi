39c20c659c3590be569beca293447fb77af9c67d
Max Torres <maxtorres487@gmail.com>
2021-04-07 19:12:10 -0400

    Created separate file SpotifyController.java to store Spotify / login routes. Login still works normally in application. Also turned many variables into public static so that SpotifyController would have access to them.

--- code/backend/src/main/java/api/ApiServer.java
+++ code/backend/src/main/java/api/ApiServer.java
@@ -2,11 +2,8 @@ package api;
 
 import dao.*;
 import exceptions.ApiError;
-import exceptions.DaoException;
-import model.*;
 import spark.Spark;
 import util.Database;
-import com.google.gson.JsonSyntaxException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import static spark.Spark.*;
@@ -15,14 +12,9 @@ import java.net.URISyntaxException;
 import java.util.*;
 import dao.MusicianDao;
 import org.sql2o.Sql2o;
-
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.SpotifyHttpManager;
-import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
-import com.wrapper.spotify.model_objects.specification.User;
-import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
-import com.wrapper.spotify.requests.data.users_profile.GetCurrentUsersProfileRequest;
 
 public class ApiServer {
 
@@ -36,12 +28,21 @@ public class ApiServer {
     public static SotwEventDao sotw_eventDao;
 
     // flag for testing locally vs. deploying
-    private static final boolean isLocalTest =
-            Boolean.parseBoolean(System.getenv("isLocal"));
+    private static final boolean isLocalTest = Boolean.parseBoolean(System.getenv("isLocal"));
+    // frontend and backend urls
+    public static String frontend_url;
+    public static String backend_url;
+
     // client id for Spotify
     private static final String client_id= "ae87181e126a4fd9ac434b67cf6f6f14";
     // Client Secret for using Spotify API (should never push to VCS)
     private static final String client_secret = System.getenv("client_secret");
+    // Redirect uri after Spotify dialog
+    public static URI redirect_uri;
+    // Spotify API variable
+    public static SpotifyApi spotifyApi;
+    // Spotify authorization code uri request
+    public static AuthorizationCodeUriRequest auth_code_uri_req;
 
     private static int getHerokuAssignedPort() {
         // Heroku stores port number as an environment variable
@@ -54,13 +55,15 @@ public class ApiServer {
     }
 
     public static void main(String[] args) throws URISyntaxException {
+
         int myPort = getHerokuAssignedPort();
         port(myPort);
         staticFiles.location("/public");
 
-        // static Gson object
+        // Initialize Gson object
         gson = new GsonBuilder().disableHtmlEscaping().create();
-        // Dao objects
+
+        // Initialize Dao objects
         musicianDao = getMusicianDao();
         bandDao = getBandDao();
         speedDateEventDao = getSpeedDateEventDao();
@@ -69,6 +72,31 @@ public class ApiServer {
         sotw_submissionDao = getSubmissionDao();
         sotw_eventDao = getEventDao();
 
+        // Store frontend and backend urls
+        if (isLocalTest) {
+            frontend_url = "http://localhost:3000";
+            backend_url = "http://localhost:4567";
+        }
+        else {
+            frontend_url = "http://bandiscover.herokuapp.com";
+            backend_url = "http://bandiscover-api.herokuapp.com";
+        }
+
+        // Initialize Spotify variables
+        redirect_uri =
+                SpotifyHttpManager.makeUri(backend_url + "/callback");
+        spotifyApi = new SpotifyApi.Builder()
+                .setClientId(client_id)
+                .setClientSecret(client_secret)
+                .setRedirectUri(redirect_uri)
+                .build();
+        auth_code_uri_req =
+                spotifyApi.authorizationCodeUri()
+                        .scope("user-read-email,user-read-private,user-top-read")
+                        .show_dialog(true)
+                        .build();
+
+        // Exception for API Error
         exception(ApiError.class, (ex, req, res) -> {
             // Handle the exception here
             Map<String, String> map = Map.of("status", ex.getStatus() + "",
@@ -78,80 +106,9 @@ public class ApiServer {
             res.type("application/json");
         });
 
-        // Set frontend and backend urls
-        String frontend_url;
-        String backend_url;
-        if (isLocalTest) {
-            frontend_url = "http://localhost:3000";
-            backend_url = "http://localhost:4567";
-        }
-        else {
-            frontend_url = "http://bandiscover.herokuapp.com";
-            backend_url = "http://bandiscover-api.herokuapp.com";
-        }
-        // Set the redirect_uri from Spotify dialog
-        final URI redirect_uri =
-                SpotifyHttpManager.makeUri(backend_url + "/callback");
-        // Spotify API variable
-        final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-                .setClientId(client_id)
-                .setClientSecret(client_secret)
-                .setRedirectUri(redirect_uri)
-                .build();
-        // authorization code uri request
-        final AuthorizationCodeUriRequest auth_code_uri_req =
-                spotifyApi.authorizationCodeUri()
-                        .scope("user-read-email,user-read-private,user-top-read")
-                        .show_dialog(true)
-                        .build();
-
-        // Redirects the window to Spotify login
-        get("/login", (req, res) -> {
-            URI uri_for_code = auth_code_uri_req.execute();
-            String uriString = uri_for_code.toString();
-            res.redirect(uriString);
-            return null;
-            //return new JSONObject("{\"link\": \""+uriString+"\"}");
-        });
-        // Where Spotify redirects after login dialog
-        get("/callback", (req, res) -> {
-
-            String error = req.queryParams("error");
-            if (error != null) { // SSO was canceled by user
-                res.redirect(frontend_url);
-                return null;
-            }
-
-            // Use authorization code to get access token and refresh token
-            String code = req.queryParams("code");
-            AuthorizationCodeRequest auth_code_req =
-                    spotifyApi.authorizationCode(code).build();
-            AuthorizationCodeCredentials auth_code_credentials =
-                    auth_code_req.execute();
-
-            // Set tokens in Spotify API Object
-            spotifyApi.setAccessToken(auth_code_credentials.getAccessToken());
-            spotifyApi.setRefreshToken(auth_code_credentials.getRefreshToken());
-
-            // get current user's info
-            final GetCurrentUsersProfileRequest getCurrentUser =
-                    spotifyApi.getCurrentUsersProfile()
-                    .build();
-            final User user = getCurrentUser.execute();
-            String name = user.getDisplayName();
-            String id = user.getId();
-
-            res.redirect(frontend_url + "/?id=" + id);
-
-            // Create user in database if not already existent
-            Musician musician = musicianDao.read(id);
-            if (musician == null) { // user has not been added to database yet
-                musicianDao.create(id, name);
-            }
-
-            return null;
-            //return new JSONObject("{\"id\": \"" + id + "\"}");
-        });
+        // Spotify / Login API routes
+        get("/login", SpotifyController.loginWithSpotify);
+        get("/callback", SpotifyController.callbackFromSpotify);
 
         // Musician routes
         get("/musicians/:id", MusicianController.getMusicianById);
@@ -207,7 +164,7 @@ public class ApiServer {
         delete("/sotwevents/submissions/:eventid/:submissionid", SOTWEventController.deleteSubmissionFromSOTWEvent);
         delete("/sotwevents/:eventid", SOTWEventController.deleteSOTWEvent);
 
-        // options request to allow for CORS
+        // options route for CORS
         options("/*", (req, res) -> {
             String headers = req.headers("Access-Control-Request-Headers");
             if (headers != null) {
@@ -220,7 +177,7 @@ public class ApiServer {
             return "OK";
         });
 
-        // CORS
+        // Set header for CORS
         before((req, res) -> {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
@@ -232,8 +189,6 @@ public class ApiServer {
 
     private static MusicianDao getMusicianDao() throws URISyntaxException{
         Sql2o sql2o = Database.getSql2o();
-        //List<Musician> musicians = DataStore.sampleMusicians();
-        //Database.createMusicianTablesWithSampleData(sql2o, musicians);
         return new Sql2oMusicianDao(sql2o);
     }
 
---
+++ code/backend/src/main/java/api/SpotifyController.java
@@ -0,0 +1,59 @@
+package api;
+
+import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import com.wrapper.spotify.model_objects.specification.User;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
+import com.wrapper.spotify.requests.data.users_profile.GetCurrentUsersProfileRequest;
+import model.Musician;
+import spark.Route;
+import java.net.URI;
+import static api.ApiServer.*;
+
+public class SpotifyController {
+
+    // Redirects the window for Spotify login dialog
+    public static Route loginWithSpotify = (req, res) -> {
+        URI uri_for_code = auth_code_uri_req.execute();
+        String uriString = uri_for_code.toString();
+        res.redirect(uriString);
+        return null;
+    };
+
+    // Spotify will redirect to here after successful login
+    public static Route callbackFromSpotify = (req, res) -> {
+
+        String error = req.queryParams("error");
+        if (error != null) { // SSO was canceled by user
+            res.redirect(frontend_url);
+            return null;
+        }
+
+        // Use authorization code to get access token and refresh token
+        String code = req.queryParams("code");
+        AuthorizationCodeRequest auth_code_req =
+                spotifyApi.authorizationCode(code).build();
+        AuthorizationCodeCredentials auth_code_credentials =
+                auth_code_req.execute();
+
+        // Set tokens in Spotify API Object
+        spotifyApi.setAccessToken(auth_code_credentials.getAccessToken());
+        spotifyApi.setRefreshToken(auth_code_credentials.getRefreshToken());
+
+        // get current user's info
+        final GetCurrentUsersProfileRequest getCurrentUser =
+                spotifyApi.getCurrentUsersProfile()
+                        .build();
+        final User user = getCurrentUser.execute();
+        String name = user.getDisplayName();
+        String id = user.getId();
+
+        res.redirect(frontend_url + "/?id=" + id);
+
+        // Create user in database if not already existent
+        Musician musician = musicianDao.read(id);
+        if (musician == null) { // user has not been added to database yet
+            musicianDao.create(id, name);
+        }
+        return null;
+    };
+}
