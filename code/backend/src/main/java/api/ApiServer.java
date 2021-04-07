321b45ec5d00fc95ffb990cf118044a685c90baf
Max Torres <maxtorres487@gmail.com>
2021-04-07 09:41:47 -0400

    Refactored Band routes

--- code/backend/src/main/java/api/ApiServer.java
+++ code/backend/src/main/java/api/ApiServer.java
@@ -170,113 +170,12 @@ public class ApiServer {
         post("/request/:senderid/:recipientid", RequestController.postFriendRequest);
         delete("request/:senderid/:recipientid/:action", RequestController.respondToRequest);
 
-        // Get all bands (optional query parameters)
-        // if searching for id, only pass 1 parameter
-        get("/bands", (req, res) -> {
-            try {
-                List<Band> bands;
-                Map<String, String[]> query = req.queryMap().toMap();
-
-                if (query.get("musicianId") != null) {
-                    bands = bandDao.readAll(query.get("musicianId")[0]);
-                } else if (query.size() > 0) {
-                    bands = bandDao.readAll(query);
-                } else {
-                    bands = bandDao.readAll();
-                }
-                return gson.toJson(bands);
-            } catch (DaoException ex) {
-                throw new ApiError(ex.getMessage(), 500);
-            }
-        });
-
-        // Get band given the id
-        get("/bands/:id", (req, res) -> {
-            try {
-                String id = req.params("id");
-                Band band = bandDao.read(id);
-                if (band == null) {
-                    throw new ApiError("Resource not found", 404); // Bad request
-                }
-                res.type("application/json");
-                return gson.toJson(band);
-            } catch (DaoException ex) {
-                throw new ApiError(ex.getMessage(), 500);
-            }
-        });
-
-        // post band
-        post("/bands", (req, res) -> {
-            try {
-                Band band = gson.fromJson(req.body(), Band.class);
-
-                String id = UUID.randomUUID().toString();
-                Set<String> members = band.getMembers();
-                Set<String> genres = band.getGenres();
-                String name = band.getName();
-                int capacity = band.getCapacity();
-
-                if (members == null) { members = new HashSet<String>(); }
-                if (genres == null) { genres = new HashSet<String>(); }
-                if (name == null) { name = "NULL"; }
-                if (capacity == 0) { capacity = 1; }
-
-                bandDao.create(id, name, capacity, genres, members);
-
-                res.status(201);
-                return gson.toJson(band);
-            } catch (DaoException ex) {
-                throw new ApiError(ex.getMessage(), 500);
-            }
-        });
-
-        // put new band members
-        put("/bands/:bid/:mid", (req, res) -> {
-            try {
-                String bandId = req.params("bid");
-                String musicianId = req.params("mid");
-                Band band = bandDao.read(bandId);
-                Musician musician = musicianDao.read(musicianId);
-                if (band == null) {
-                    throw new ApiError("Resource not found", 404);
-                }
-                if (musician == null) {
-                    throw new ApiError("Resource not found", 404);
-                }
-                if (!(musician.getId()).equals(musicianId)) {
-                    throw new ApiError("musician ID does not match the resource identifier", 400);
-                }
-                if (!(band.getId()).equals(bandId)) {
-                    throw new ApiError("band ID does not match the resource identifier", 400);
-                }
-
-                Band b = bandDao.add(bandId, musicianId);
-                if (b == null) {
-                    throw new ApiError("Resource not found", 404);
-                }
-
-                return gson.toJson(b);
-            } catch (DaoException | JsonSyntaxException ex) {
-                throw new ApiError(ex.getMessage(), 500);
-            }
-        });
-
-        // remove band member from band
-        delete("/bands/:bid/:mid", (req, res) -> {
-            try {
-                String bandId = req.params("bid");
-                String musicianId = req.params("mid");
-                Band band = bandDao.remove(bandId, musicianId);
-                if (band == null) {
-                    throw new ApiError("Resource not found", 404);
-                }
-
-                res.type("application/json");
-                return gson.toJson(band);
-            } catch (DaoException ex) {
-                throw new ApiError(ex.getMessage(), 500);
-            }
-        });
+        // Band routes
+        get("/bands", BandController.getAllBands);
+        get("/bands/:id", BandController.getBandById);
+        post("/bands", BandController.postBand);
+        put("/bands/:bid/:mid", BandController.putBandMember);
+        delete("/bands/:bid/:mid", BandController.deleteMember);
 
         // Get all SpeedDateEvents
         get("/speeddateevents", (req, res) -> {
@@ -781,7 +680,6 @@ public class ApiServer {
         Sql2o sql2o = Database.getSql2o();
         return new Sql2oBandDao(sql2o);
     }
-
 
     private static Sql2oSpeedDateEventDao getSpeedDateEventDao() throws URISyntaxException {
         Sql2o sql2o = Database.getSql2o();
