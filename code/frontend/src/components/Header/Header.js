509fc9bf31d28316e0690266bd4c9bb41e97eb79
ramcha24 <ramcha1994@gmail.com>
2021-04-13 12:34:28 -0400

    added header failsafe navbar

--- code/frontend/src/components/Header/Header.js
+++ code/frontend/src/components/Header/Header.js
@@ -47,6 +47,29 @@ const Header = () => {
           </Navbar>
         </div>
     )
+  } else { //Header edge case that should never happen
+    return (
+        <div>
+          <Navbar expand="lg" variant="dark" bg="dark">
+            <Navbar.Brand href="/">
+              banDi
+            </Navbar.Brand>
+            <Navbar.Brand className="mx-auto">
+              Welcome!
+            </Navbar.Brand>
+
+            <div className={styles.navDropdownTitle}>
+              <NavDropdown title="My Account">
+                <div className={styles.navDropdownItems}>
+                  <NavDropdown.Item href="#myprofile">Profile</NavDropdown.Item>
+                  <NavDropdown.Item href="#mychats">Chats</NavDropdown.Item>
+                  <NavDropdown.Item href="#signin" onClick={handleLogout}>Log Out</NavDropdown.Item>
+                </div>
+              </NavDropdown>
+            </div>
+          </Navbar>
+        </div>
+    )
   }
 
 
