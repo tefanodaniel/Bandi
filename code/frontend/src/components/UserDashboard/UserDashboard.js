333c1b411618161524db0ccf4131f33820f3fdf5
Matthew Ost <most1@jhu.edu>
2021-04-15 00:07:28 -0400

    fixed logout bug in user dashboard

--- code/frontend/src/components/UserDashboard/UserDashboard.js
+++ code/frontend/src/components/UserDashboard/UserDashboard.js
@@ -137,17 +137,17 @@ class UserDashboard extends React.Component {
                         </TabList>
 
                         <TabPanel>
-                            <h2>Name: {userInfo.name}</h2>
-                            <h4>Location: {userInfo.location === "NULL" ? "" : userInfo.location}</h4>
-                            <h4>Experience: {userInfo.experience === "NULL" ? "" : userInfo.experience}</h4>
+                            <h2>Name: {userInfo?.name}</h2>
+                            <h4>Location: {userInfo?.location === "NULL" ? "" : userInfo?.location}</h4>
+                            <h4>Experience: {userInfo?.experience === "NULL" ? "" : userInfo?.experience}</h4>
                             <div>
-                                <h4>Instruments: {userInfo.instruments ? userInfo.instruments.join(", ") : ""}</h4>
+                                <h4>Instruments: {userInfo?.instruments ? userInfo.instruments.join(", ") : ""}</h4>
                             </div>
                             <div>
-                                <h4>Genres: {userInfo.genres ? userInfo.genres.join(", ") : ""}</h4>
+                                <h4>Genres: {userInfo?.genres ? userInfo.genres.join(", ") : ""}</h4>
                             </div>
                             <div>
-                                <h4>Links: {userInfo.links ? userInfo.links.map((link, i) => <a href={link}>{link}</a>) : ""}</h4>
+                                <h4>Links: {userInfo?.links ? userInfo.links.map((link, i) => <a href={link}>{link}</a>) : ""}</h4>
                             </div>
                             <Button onClick={() => { this.props.history.push('/edit-user-info');}}>Edit Profile</Button>
                         </TabPanel>
@@ -159,7 +159,7 @@ class UserDashboard extends React.Component {
 
 
                         <TabPanel>
-                            <h3>My friends ({userInfo.friends?.length})</h3>
+                            <h3>My friends ({userInfo?.friends?.length})</h3>
                             {this.renderFriendListForMusician()}
                             <h3>Friend requests ({this.state.pending_incoming_requests?.length})</h3>
                             {this.renderIncomingRequestList()}
