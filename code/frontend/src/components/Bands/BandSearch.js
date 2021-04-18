4b3d083dbff66c92e59b917c2d12cb2d1c47693b
nkjohnson16 <nkjohnson16@gmail.com>
2021-04-17 21:26:51 -0400

    updated bandsearch to match updated musiciansearch

--- code/frontend/src/components/Bands/BandSearch.js
+++ code/frontend/src/components/Bands/BandSearch.js
@@ -1,83 +1,50 @@
-import React from 'react';
-import {shallowEqual, useDispatch, useSelector} from 'react-redux';
+import 'react-tabs/style/react-tabs.css';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {Container, Row, Col} from "react-bootstrap";
-//import { newQuery, clearQuery } from "../actions/band_actions";
-import { Button } from "react-bootstrap";
+import BandSearchResults from "./BandSearchResults";
+import BandSearchControls from "./BandSearchControls";
+import Header from "../Header/Header";
+import SubHeader from "../Header/SubHeader";
+import {bandi_styles} from "../../styles/bandi_styles";
 
-const BandSearch = () => {
-    const dispatch = useDispatch();
-    let user = useSelector((state) => state.user_reducer, shallowEqual);
-    let queryparams = {};
-
-    const addnamequery = (e) => {
-        let input = e.target.value;
-        queryparams.name = input
+class BandSearch extends Component {
+    contructor(props) {
+        super(props);
     }
 
-    const addgenrequery = (e) => {
-        let input = e.target.value;
-        queryparams.genre = input
-    }
-
-    const adddistancequery = (e) => {
-        let input = e.target.value;
-        queryparams.distance = input
-    }
-
-    const addcapacityquery = (e) => {
-        let input = e.target.value;
-        queryparams.capacity = input
-    }
-
-    const SubmitQuery = () => {
-        if(Object.keys(queryparams)===0) {
-            dispatch(clearQuery)
+    render() {
+        if(true) {
+            return (
+                <div style={bandi_styles.discover_background}>
+                    <Header/>
+                    <SubHeader text={"Find your future band here!"}/>
+                    <Container className="mx-auto" fluid>
+                        <Row>
+                            <Col className="col-sm-3">
+                                <BandSearchControls />
+                            </Col>
+                            <Col className="col-sm-9">
+                                <BandSearchResults />
+                            </Col>
+                        </Row>
+                    </Container>
+                    <div></div>
+                </div>
+            );
         } else {
-            queryparams.id = user.id
-            dispatch(newQuery(queryparams))
+            return (
+                <div>
+                    <h1> Here are your search results: </h1>
+                    <h3> Loading...</h3>
+                </div>
+            );
         }
     }
-
-    return (
-    /** Add formatting for the band search on the webpage
-    */
-        <Container fluid>
-            <Row className="justify-content-sm-left" style={{marginTop:"20px"}}>
-                <Col className="col-sm-5">
-                    <h5> Name:</h5>
-                </Col>
-                <Col className="justify-content-sm-left" style={{minWidth: "175px", textAlign: "center"}}>
-                    <input onChange={e => {addnamequery(e);}} style={{width: "120%"}} placeholder='Search by Name' type='text'/>
-                </Col>
-            </Row>
-            <Row className="justify-content-sm-left" style={{marginTop:"20px"}}>
-                <Col className="col-sm-5">
-                    <h5> Genre:</h5>
-                </Col>
-                <Col>
-                    <div className="col-sm-7" style={{minWidth:"175px", textAlign:"center"}}>
-                        <input onChange={e => {addgenrequery(e);}} style={{width:"120%"}} placeholder='Search by Genre' type='text'/>
-                    </div>
-                </Col>
-            </Row>
-            <Row className="justify-content-sm-left" style={{marginTop:"20px"}}>
-                <Col className="col-sm-5">
-                    <h5 Within Distance (miles):</h5>
-                </Col>
-                <div className="col-sm-7" style={{minWidth:"175px", textAlign:"center"}}>
-                    <input onChange={e =>{adddistancequery(e);}} style={{width:"120%"}} placeholder='500' type='text'/>
-                </div>
-            </Row>
-            <Row className="justify-content-sm-left" style={{marginTop:"20px"}}>
-                <Col className="col-sm-5">
-                </Col>
-                <div className="col-sm-7" style={{textAlign: "center"}}>
-                    <Button variant="primary" onClick={SubmitQuery}>Submit</Button>
-                </div>
-            </Row>
-        </Container>
-    )
-
 }
 
-export default BandSearch
+function mapStateToProps(state) {
+    return {state};
+}
+
+export default connect(mapStateToProps)(BandSearch);
